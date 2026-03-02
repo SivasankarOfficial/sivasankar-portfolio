86369739cde69fd7abbdcc4c6cc5fe7424a47179
Sivasankar <sivasankar@Sivasankars-MacBook-Air.local>
2026-03-02 16:06:32 +0530

    profile updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import resumePdf from '../assets/Sivasankar_Resume.pdf';
 import { portfolioData } from '../data/portfolioData';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import ProfilePic from '../components/ProfilePic';
+import profilePic from "../assets/profile.jpeg";
 
 const Hero = () => {
     const { name, title, overview } = portfolioData.personalInfo;
@@ -76,7 +76,7 @@ const Hero = () => {
                     </div>
 
                     <div className="order-1 lg:order-2 flex justify-center lg:justify-end">
-                        <ProfilePic src={"src/assets/profile.jpeg"} />
+                        <ProfilePic src={profilePic} />
                     </div>
                 </div>
             </div>
